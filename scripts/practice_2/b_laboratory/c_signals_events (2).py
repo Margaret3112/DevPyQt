c8d564b63eede4be3f5164f1f051ab60cdf8133d
79218 <lobankova_ritusy@mail.ru>
2025-05-28 16:57:49 +0300

    prac3

---
+++ scripts/practice_2/b_laboratory/c_signals_events (2).py
@@ -0,0 +1,83 @@
+"""
+Реализация программу проверки состояния окна:
+Форма для приложения (ui/c_signals_events_form.ui)
+
+Программа должна обладать следующим функционалом:
+
+1. Возможность перемещения окна по заданным координатам.
+2. Возможность получения параметров экрана (вывод производить в plainTextEdit + добавлять время).
+    * Кол-во экранов
+    * Текущее основное окно
+    * Разрешение экрана
+    * На каком экране окно находится
+    * Размеры окна
+    * Минимальные размеры окна
+    * Текущее положение (координаты) окна
+    * Координаты центра приложения
+    * Отслеживание состояния окна (свернуто/развёрнуто/активно/отображено)
+3. Возможность отслеживания состояния окна (вывод производить в консоль + добавлять время).
+    * При перемещении окна выводить его старую и новую позицию
+    * При изменении размера окна выводить его новый размер
+"""
+
+
+from PySide6 import QtWidgets, QtCore
+from ui.c_signals_events_form import Ui_Form
+
+
+class Window(QtWidgets.QWidget, Ui_Form):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.w_screen, self.h_screen = QtWidgets.QApplication.primaryScreen().size().toTuple()
+        self.setupUi(self)
+        self.spinBoxX.setMaximum(self.w_screen-self.size().width())
+        self.spinBoxY.setMaximum(self.h_screen - self.size().height())
+        self.spinBoxY.setValue(500)
+        self.spinBoxX.setValue(500)
+        print(self.pos().toTuple())
+        # self.spinBoxX.setValue(int(self.x()))
+        # self.spinBoxY.setValue(int(self.y()))
+
+        self.initSignals()
+
+        # self.move(0, 0)
+
+
+    def initSignals(self):
+        self.pushButtonGetData.clicked.connect(self.get_state_data)
+        self.pushButtonMoveCoords.clicked.connect(lambda: self.move(self.spinBoxX.value(), self.spinBoxY.value()))
+
+
+
+    def get_state_data(self):
+        # print(QtWidgets.QApplication.screens())
+        data = f"""
+        Кол-во экранов: {len(QtWidgets.QApplication.screens())}
+        Основное окно: {QtWidgets.QApplication.primaryScreen().name()}
+        Разрешение экрана: Ширина = {self.w_screen}, Высота = {self.h_screen}
+        Размеры окна: {self.size().width()} x {self.size().height()}
+        Минимальный размер: {self.minimumSize().toTuple()}
+        Максимальный размер: {self.maximumSize().toTuple()}
+        Координаты окна: {self.x()}, {self.y()}
+        Координаты окна: {self.pos().toTuple()}
+        Центр: {self.rect().center().toTuple()}
+        Состояние: {self.windowState().name}
+        """
+        self.plainTextEdit.appendPlainText(data)
+
+    def resizeEvent(self, event, /):
+        print(self.size())
+        return super().resizeEvent(event)
+
+
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication()
+
+    window = Window()
+    window.show()
+
+    app.exec()
--- scripts/practice_3/b_laboratory/b_systeminfo_widget.py
+++ scripts/practice_3/b_laboratory/b_systeminfo_widget.py
@@ -11,3 +11,36 @@
 5. установку времени задержки сделать "горячей", т.е. поток должен сразу
 реагировать на изменение времени задержки
 """
+from PySide6 import QtWidgets
+from a_threads import SystemInfo
+from ui.b_system import Ui_Form
+
+
+class Window(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.thread_check = SystemInfo()
+        self.thread_check.systemInfoReceived.connect(self.cpuram_info)
+        self.thread_check.delay = 1
+        self.thread_check.start()
+        self.ui.lineEdit.textChanged.connect(self.new_delay)
+
+    def cpuram_info (self, data):
+        cpu, ram = data
+        self.ui.lineEdit_2.setText(f"{cpu}%")
+        self.ui.lineEdit_3.setText(f"{ram}%")
+
+    def new_delay(self, value: str):
+            delay = float(value)
+            if delay > 0:
+                self.thread_check.set_delay = delay
+
+
+
+if __name__ == '__main__':
+            app = QtWidgets.QApplication()
+            window = Window()
+            window.show()
+            app.exec()
